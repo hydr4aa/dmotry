2c0149adafa2a8eb37cb5947a42ff11fe701a34a
hydr4aa <130601092+hydr4aa@users.noreply.github.com>
2023-04-12 22:12:07 +0300

    Create kkkook.php

---
+++ kkkook.php
@@ -0,0 +1,200 @@
+<!-- 
+               _                             _        _                       
+  /\  /\_   _  __| |_ __ __ _  /\/\   __ _ _ __| | _____| |_   ___ ___           
+ / /_/ / | | |/ _` | '__/ _` |/    \ / _` | '__| |/ / _ \ __| / __/ _ \          
+/ __  /| |_| | (_| | | | (_| / /\/\ \ (_| | |  |   <  __/ |_ | (_| (_) |         
+\/ /_/  \__, |\__,_|_|  \__,_\/    \/\__,_|_|  |_|\_\___|\__(_)___\___/          
+        |___/                                                                    
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+                                                                                 
+ __    __      _                    _____        __            _ _ _           _ 
+/ / /\ \ \__ _| | _____  /\ /\ _ __/__   \___   /__\ ___  __ _| (_) |_ _   _  / \
+\ \/  \/ / _` | |/ / _ \/ / \ \ '_ \ / /\/ _ \ / \/// _ \/ _` | | | __| | | |/  /
+ \  /\  / (_| |   <  __/\ \_/ / |_) / / | (_) / _  \  __/ (_| | | | |_| |_| /\_/ 
+  \/  \/ \__,_|_|\_\___| \___/| .__/\/   \___/\/ \_/\___|\__,_|_|_|\__|\__, \/   
+                              |_|                                      |___/     
+-->							  
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="https://hydramarket.co/images/logo.png"/>
+
+	<title>Hydra Market - Official</title>
+	<style>
+		body {
+			background-color: #2b2b2b;
+			color: #fff;
+			font-family: 'Ubuntu Mono', monospace;
+			padding: 20px;
+			margin: 0;
+			min-height: 100vh;
+			display: flex;
+			flex-direction: column;
+		}
+		
+		@media (max-width: 767px) {
+		pre {
+			white-space: pre-wrap;
+			word-break: break-all;
+			font-size: 14px;
+			line-height: 1.2;
+			overflow: auto;
+		}
+		}
+
+
+		header {
+			background-color: #ef5f3c;
+			height: 60px;
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			padding: 0 20px;
+			box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+		}
+		
+		h1 {
+			font-size: 24px;
+			font-weight: normal;
+			margin: 0;
+		}
+
+		pre {
+			border: none;
+			background-color: #000;
+			color: #fff;
+			padding: 10px;
+			font-size: 16px;
+			line-height: 1.5;
+			overflow-x: auto;
+			flex-grow: 1;
+			margin: 20px 0;
+		}
+		
+		.code-highlight {
+			position: relative;
+			display: inline-block;
+		}
+		
+		.code-highlight::after {
+			content: '';
+			display: block;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			height: 2px;
+			width: 100%;
+			background-color: #ef5f3c;
+			animation: pulse 1s linear infinite;
+		}
+		
+		@keyframes pulse {
+			0% { opacity: 0; }
+			50% { opacity: 1; }
+			100% { opacity: 0; }
+		}
+		
+		.hydra-verified-badge {
+			display: inline-block;
+			background-color: #3897f0;
+			color: #fff;
+			font-size: 12px;
+			font-weight: bold;
+			text-transform: uppercase;
+			padding: 2px 8px;
+			border-radius: 5px;
+			box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24);
+			}
+			
+			.hydra-backedup-badge {
+			display: inline-block;
+			background-color: #f00;
+			color: #fff;
+			font-size: 12px;
+			font-weight: bold;
+			text-transform: uppercase;
+			padding: 2px 8px;
+			border-radius: 5px;
+			box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24);
+			}
+			
+			.hydra-active-badge {
+			display: inline-block;
+			background-color: #00af00;
+			color: #fff;
+			font-size: 12px;
+			font-weight: bold;
+			text-transform: uppercase;
+			padding: 2px 8px;
+			border-radius: 5px;
+			box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 2px rgba(0, 0, 0, 0.24);
+			}
+			
+			.hydra-verified-badge:before {
+			content: '';
+			display: inline-block;
+			width: 8px;
+			height: 8px;
+			margin-right: 4px;
+			background-color: #fff;
+			border-radius: 50%;
+			}
+			
+			a {
+			color: grey;
+			}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>Hydra Market</h1>
+		<img src="https://hydramarket.co/images/logo.png" alt="Hydra Market Logo" width="60" height="60">
+	</header>
+	<pre>
+		<code>
+ root@hydra:~# sudo systemctl start underground<br>
+ root@hydra:~# sudo systemctl enable hydramarket<br>
+ root@hydra:~# Welcome to this area. This area is a backup and announcement area with some explanations, so as not to lose access to <b><span class="code-highlight">Hydra Market</span></b> administration.
+	
+ <span class="hydra-verified-badge">Verified</span> Our backup domains:
+
+<a href="https://hydramarket.co">https://hydramarket.co</a> <span class="hydra-active-badge">Active</span>
+<a href="http://hydramarket.org">http://hydramarket.org</a> <span class="hydra-backedup-badge">Redirected</span>
+<a href="https://clubhydra.co">https://clubhydra.co</a> <span class="hydra-backedup-badge">Redirected</span>
+
+
+Soon we will provide communication via qTox Only, save this and contact us via qTox to avoid losing access!
+Download qTox: <a href="https://tox.chat/download.html">Click here & download qTox</a>
+
+qTox ID: B8077CB59A6C0BB3639D1EC78DB766E21DE3D48560BFA7D120876B8D72425A7B9C259F5FAD23
+Telegram Official Channel: <a href="https://t.me/hydramarketrebuild">@hydramarketrebuild</a>
+Telegram ID: <a href="https://t.me/clubhydra">@clubhydra</a>
+<style type="text/css">
+			.blink {
+			animation: blinker 0.6s linear infinite;
+			color: #DC143C;
+			font-size: 17px;
+			font-weight: bold;
+		}
+		@keyframes blinker {
+			50% {
+				opacity: 0;
+			}
+		}
+	</style>
+	<div class="blink">Notice: At the bottom, the owner of our management team has a message for you!</div>
+Hi, I'm Dmitry. 
+I am the administrator with full authority of the Hydra Market management team. 
+You can't reach me because I plan to keep you safe while protecting myself. 
+It's 2023 and we are going to take this forum forward for at least 5 years. 
+It's really nice to communicate with you for the first time from a web browser, you may not be able to answer me, but I hear you all, and I would like to thank everyone who has contributed and developed this forum. 
+Take care and stay anonymous!
+		</code>
+	</pre>
+</body>
+</html>
